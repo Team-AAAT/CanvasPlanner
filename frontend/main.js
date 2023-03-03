9faf66582f2c1f68faee3d912a05416a57bb4a03
WatsonMLDev <watsonc5251@gmail.com>
2023-03-03 12:11:51 -0600

    project initiation

---
+++ frontend/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, ipcMain, ipcRenderer, dialog} = require("electron")
+const electron = require("electron")
+var mainWindow
+
+function createWindow() {
+    const display = electron.screen.getPrimaryDisplay()
+    const { width, height } = display.bounds
+    mainWindow.setBounds({
+        x: -8,
+        y: 0,
+        width: width,
+        height: height})
+    mainWindow.setAlwaysOnTop(false)
+    mainWindow.loadFile('views/index.html')
+}
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow( {
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+    createWindow() // creates the new window
+})
+ipcMain.on("view2", (event, arg) => {
+    mainWindow.loadFile('views/view2.html')
+})
+ipcMain.on("home", (event, arg) => {
+    mainWindow.loadFile('views/index.html')
+})
---
+++ frontend/scripts/view2.js
@@ -0,0 +1,5 @@
+const { ipcRenderer  } = require ("electron");
+
+function view2(){
+    ipcRenderer.send("home");
+}
