b2da391832b51c411918676ca3f1a190205923ca
WatsonMLDev <watsonc5251@gmail.com>
2023-03-15 10:33:51 -0500

    first working version of backend server processing. using Javalin, ical4j, gson, jackson-core, kotlin-stdlib,slf4j, and jsondb

--- backend/Controller.java
+++ backend/Controller.java
@@ -4,17 +4,23 @@ import models.Calendar;
 
 public class Controller {
     public static void main(String[] args) {
+
+        Javalin app = Javalin.create().start(5000);
+
+        app.post("/makeCalendarEvent", ctx ->{
+
+            Calendar tempEvent = new Calendar(1,"test", "this is a temp calendar event");
+
+            Attribute<Integer> tempAttribute= new Attribute<>(1, "Date", 123, Integer.class);
+            tempEvent.addIntAttribute(tempAttribute);
+
+        });
+
+        app.get("/output", ctx -> {
+            // some code
+            ctx.json("{ \"name\": \"John\", \"age\": 30 }}");
+        });
+
         System.out.println("Server is running...");
-
-        try (Javalin app = Javalin.create().start(8080)){
-            app.post("/makeCalendarEvent", ctx ->{
-
-                Calendar tempEvent = new Calendar(1,"test", "this is a temp calendar event");
-
-                Attribute<Integer> tempAttribute= new Attribute<Integer>(1, "Date", 123, Integer.class);
-                tempEvent.addIntAttribute(tempAttribute);
-
-            });
-        }
     }
 }
