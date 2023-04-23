31b66da5d1309553fdaa54e815fb9d96a392d857
Austin Besong <ajb7227@psu.edu>
2023-04-23 16:27:16 -0400

    moved button functions to index.js
    also improved textbox formatting seen in start date and end date

--- src/main/resources/scripts/index.js
+++ src/main/resources/scripts/index.js
@@ -3,3 +3,30 @@ const { ipcRenderer  } = require ("electron");
 function view2(){
     ipcRenderer.send("view2");
 }
+
+function changeToWeekView(){
+    document.getElementsByClassName("monthViewContainer").style.display = 'none';
+    document.getElementsByClassName("yearViewContainer").style.display = 'none';
+    document.getElementsByClassName("weekViewContainer").style.display = 'block';
+}
+
+function changeToMonthView(){
+    document.getElementsByClassName("weekViewContainer").style.display = 'none';
+    document.getElementsByClassName("yearViewContainer").style.display = 'none';
+    document.getElementsByClassName("monthViewContainer").style.display = 'block';
+}
+
+function changeToYearView(){
+    document.getElementsByClassName("weekViewContainer").style.display = 'none';
+    document.getElementsByClassName("monthViewContainer").style.display = 'none';
+    document.getElementsByClassName("yearViewContainer").style.display = 'block';
+}
+
+function addEventView(){
+    document.getElementsByClassName("selectedItemContainer").style.display = 'none';
+    document.getElementsByClassName("addItemContainer").style.display = 'block';
+}
+
+function submitNewEvent(){
+    
+}
--- src/main/resources/scripts/view2.js
+++ src/main/resources/scripts/view2.js
@@ -4,26 +4,5 @@ function view2(){
     ipcRenderer.send("home");
 }
 
-function changeToWeekView(){
-    document.getElementsByClassName("monthViewContainer").style.display = 'none';
-    document.getElementsByClassName("yearViewContainer").style.display = 'none';
-    document.getElementsByClassName("weekViewContainer").style.display = 'block';
-}
 
-function changeToMonthView(){
-    document.getElementsByClassName("weekViewContainer").style.display = 'none';
-    document.getElementsByClassName("yearViewContainer").style.display = 'none';
-    document.getElementsByClassName("monthViewContainer").style.display = 'block';
-}
-
-function changeToYearView(){
-    document.getElementsByClassName("weekViewContainer").style.display = 'none';
-    document.getElementsByClassName("monthViewContainer").style.display = 'none';
-    document.getElementsByClassName("yearViewContainer").style.display = 'block';
-}
-
-function addEventView(){
-    document.getElementsByClassName("selectedItemContainer").style.display = 'none';
-    document.getElementsByClassName("addItemContainer").style.display = 'block';
-}
 
